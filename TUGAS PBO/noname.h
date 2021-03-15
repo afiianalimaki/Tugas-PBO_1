c92db49db50fe5dae5d530d781ffa3589cea0111
afiianalimaki <72922746+afiianalimaki@users.noreply.github.com>
2021-03-15 20:59:24 +0700

    Add files via upload

---
+++ TUGAS PBO/noname.h
@@ -0,0 +1,81 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version Jun 17 2015)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO "NOT" EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef __NONAME_H__
+#define __NONAME_H__
+
+#include <wx/artprov.h>
+#include <wx/xrc/xmlres.h>
+#include <wx/string.h>
+#include <wx/frame.h>
+#include <wx/gdicmn.h>
+#include <wx/font.h>
+#include <wx/colour.h>
+#include <wx/settings.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
+#include <wx/sizer.h>
+#include <wx/statbox.h>
+
+///////////////////////////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class MyFrame1
+///////////////////////////////////////////////////////////////////////////////
+class MyFrame1 : public wxFrame 
+{
+	private:
+	
+	protected:
+	
+	public:
+		
+		MyFrame1( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 500,300 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
+		
+		~MyFrame1();
+	
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class MyPanel1
+///////////////////////////////////////////////////////////////////////////////
+class MyPanel1 : public wxPanel 
+{
+	private:
+	
+	protected:
+	
+	public:
+		
+		MyPanel1( wxWindow* parent, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 500,300 ), long style = wxTAB_TRAVERSAL ); 
+		~MyPanel1();
+	
+};
+
+///////////////////////////////////////////////////////////////////////////////
+/// Class MyFrame1
+///////////////////////////////////////////////////////////////////////////////
+class MyFrame1 : public wxFrame 
+{
+	private:
+	
+	protected:
+		wxStaticText* m_staticText3;
+		wxStaticText* m_staticText4;
+		wxStaticText* m_staticText5;
+		wxStaticText* m_staticText6;
+	
+	public:
+		
+		MyFrame1( wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = wxEmptyString, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 500,300 ), long style = wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL );
+		
+		~MyFrame1();
+	
+};
+
+#endif //__NONAME_H__
